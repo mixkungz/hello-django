a6dfce378b3533c2b89fa43f704f89dc393c4198
Phachara Kamthong <pcr.mixkungz@hotmail.com>
2019-01-04 15:00:57 +0700

    :card_file_box: Add questionnaire models and migrate it.

--- questionnaire/models.py
+++ questionnaire/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Question(models.Model):
+    id = models.AutoField(primary_key=True)
+    question = models.CharField(max_length=500)
+    created_at = models.DateTimeField(default=timezone.now)
+
+
+class Answer(models.Model):
+    question_id = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answer = models.CharField(max_length=100)
+
+
