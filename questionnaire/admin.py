faebf4c59faa9eac41a22c5cf6649ab64b3b6384
Phachara Kamthong <pcr.mixkungz@hotmail.com>
2019-01-04 15:59:47 +0700

    :card_file_box: Change model structure and register new structure to admin.

--- questionnaire/admin.py
+++ questionnaire/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Question, Choices, QuestionChoice, Answer
 # Register your models here.
+
+admin.site.register(Question)
+admin.site.register(Choices)
+admin.site.register(QuestionChoice)
+admin.site.register(Answer)
--- questionnaire/models.py
+++ questionnaire/models.py
@@ -7,14 +7,31 @@ class Question(models.Model):
     question = models.CharField(max_length=500)
     created_at = models.DateTimeField(default=timezone.now)
 
+    def __str__(self):
+        return self.question
+
+
+class Choices(models.Model):
+    id = models.AutoField(primary_key=True)
+    choice = models.TextField()
+
+    def __str__(self):
+        return self.choice
+
 
 class QuestionChoice(models.Model):
     question_id = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice = models.TextField()
+    choice_id = models.ForeignKey(Choices, on_delete=models.CASCADE, default=0)
+
+    class Meta:
+        unique_together = (('question_id', 'choice_id'))
 
 
 class Answer(models.Model):
     question_id = models.ForeignKey(Question, on_delete=models.CASCADE)
-    answer = models.CharField(max_length=100)
+    choice_id = models.ForeignKey(Choices, on_delete=models.CASCADE, default=0)
 
+    class Meta:
+        unique_together = (('question_id', 'choice_id'))
+    
 
