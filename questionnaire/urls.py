44a3239c2bc3216ad31a81a7f421820eade99a87
Phachara Kamthong <pcr.mixkungz@hotmail.com>
2019-01-04 14:32:23 +0700

    :sparkles: Create answers template and mapping to views to urls.

--- questionnaire/urls.py
+++ questionnaire/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('questions', views.init_question_page, name='Initial Page'),
+    path('answers', views.init_answer_page, name='Initial Page'),
 ]
--- questionnaire/views.py
+++ questionnaire/views.py
@@ -3,4 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 
 def init_question_page(request):
-    return render(request,'question_list.html')
+    return render(request, 'question_list.html')
+
+
+def init_answer_page(request):
+    return render(request, 'answers.html')
