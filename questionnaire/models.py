321683feb8d7e788ce68735473e94441579da734
Phachara Kamthong <pcr.mixkungz@hotmail.com>
2019-01-08 10:56:55 +0700

    :card_file_box: Add created_at, updated_at to every model.

--- questionnaire/models.py
+++ questionnaire/models.py
@@ -1,3 +1,4 @@
+# Django
 from django.db import models
 from django.utils import timezone
 
@@ -5,7 +6,8 @@ from django.utils import timezone
 class Question(models.Model):
     id = models.AutoField(primary_key=True)
     question = models.CharField(max_length=500)
-    created_at = models.DateTimeField(default=timezone.now)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.question
@@ -14,6 +16,10 @@ class Question(models.Model):
 class Choices(models.Model):
     id = models.AutoField(primary_key=True)
     choice = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    
 
     def __str__(self):
         return self.choice
@@ -22,6 +28,8 @@ class Choices(models.Model):
 class QuestionChoice(models.Model):
     question_id = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_id = models.ForeignKey(Choices, on_delete=models.CASCADE, default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         unique_together = (('question_id', 'choice_id'))
@@ -30,6 +38,8 @@ class QuestionChoice(models.Model):
 class Answer(models.Model):
     question_id = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_id = models.ForeignKey(Choices, on_delete=models.CASCADE, default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         unique_together = (('question_id', 'choice_id'))
