803518f11b78e3336d5c5bf4ac3a5327ccf2ed44
Phachara Kamthong <pcr.mixkungz@hotmail.com>
2019-01-08 16:06:33 +0700

    :pencil2: Change namespace.

--- questionnaire/urls.py
+++ questionnaire/urls.py
@@ -5,6 +5,6 @@ from django.urls import path
 from questionnaire.views import QuestionView,AnswerView
 
 urlpatterns = [
-    path('questions', QuestionView.as_view(), name='Initial Page'),
-    path('answers', AnswerView.as_view(), name='Initial Page'),
+    path('questions', QuestionView.as_view(), name='question_view'),
+    path('answers', AnswerView.as_view(), name='answer_view'),
 ]
