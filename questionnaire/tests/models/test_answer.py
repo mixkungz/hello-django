90df1bc7c17ff024909a0ee79ba446f13a82ab4d
Phachara Kamthong <pcr.mixkungz@hotmail.com>
2019-01-08 17:39:58 +0700

    :zap: Add test page contain.

--- questionnaire/tests/models/test_answer.py
+++ questionnaire/tests/models/test_answer.py
@@ -13,6 +13,7 @@ class AnswerTest(TestCase):
         choice5 = Choices.objects.create(choice='-22')
         choice6 = Choices.objects.create(choice='-222')
         choice7 = Choices.objects.create(choice='-2222')
+        
         #Add Choice into Question
         question1.choices.add(choice1, choice2, choice3, choice4)
         question2.choices.add(choice2, choice3, choice4, choice5)
@@ -21,5 +22,5 @@ class AnswerTest(TestCase):
         Answer.objects.create(question_id=question2, choice_id=choice2)
 
     def test_answer_that_has_question_id_1_and_has_choice_id_1_must_not_none(self):
-        answer = Answer.objects.get(question_id=1,choice_id=1)
+        answer = Answer.objects.get(question_id=1, choice_id=1)
         self.assertNotEqual(answer, None)
--- questionnaire/tests/views/test_question_view.py
+++ questionnaire/tests/views/test_question_view.py
@@ -1,12 +1,21 @@
 from django.urls import reverse
 from django.test import TestCase, Client
+from questionnaire.models import Question, Choices, Answer
 
 class QuestionViewTest(TestCase):
     # def setUp(self):
     #     self.client = Client()
 
     def test_question_view_get_method(self):
+        question1 = Question.objects.create(question='Germany = ?')
+        choice1 = Choices.objects.create(choice='2')
+        choice2 = Choices.objects.create(choice='22')
+        choice3 = Choices.objects.create(choice='2222')
+        choice4 = Choices.objects.create(choice='222')
+        question1.choices.add(choice1, choice2, choice3, choice4)
+
         response = self.client.get(reverse('question_view'))
-        print(response)
+
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'question_list.html')
+        self.assertContains(response, 'Germany = ?')
