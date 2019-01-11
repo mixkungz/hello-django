e6eac30e149c9612bfa7ae20c50f8e5e639c0b95
Phachara Kamthong <pcr.mixkungz@hotmail.com>
2019-01-11 15:18:13 +0700

    :white_check_mark: Update model tests.

--- questionnaire/questionnaire/tests/models/test_choice.py
+++ questionnaire/questionnaire/tests/models/test_choice.py
@@ -1,17 +1,20 @@
 from django.test import TestCase
 from questionnaire.models import Choices
 
+
 class ChoicesTest(TestCase):
     def setUp(self):
         Choices.objects.create(choice='uniqlo')
         Choices.objects.create(choice='h&m')
 
-    def test_choice_uniqlo_should_has_id_1(self):
-        uniqlo = Choices.objects.get(choice='uniqlo')
-        self.assertEqual(uniqlo.id, 1)
+    def test_last_choice_should_has_choice_name_uniqlo(self):
+        last_choice = Choices.objects.last()
+        self.assertEqual(last_choice.choice, 'uniqlo')
 
-    def test_choice_handm_should_has_id_2(self):
-        ham = Choices.objects.get(choice='h&m')
-        self.assertEqual(ham.id, 2)
+    # def test_choice_uniqlo_should_has_id_1(self):
+    #     uniqlo = Choices.objects.get(choice='uniqlo')
+    #     self.assertEqual(uniqlo.id, 1)
 
-
+    # def test_choice_handm_should_has_id_2(self):
+    #     ham = Choices.objects.get(choice='h&m')
+    #     self.assertEqual(ham.id, 2)
